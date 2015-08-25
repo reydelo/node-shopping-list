c1cf2979610906b61749ba9e85e841c00a2639fc
reydelo <reyna.deloge@gmail.com>
2015-08-25 16:48:44 -0600

    add ItemLibrary

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,5 +1,33 @@
 var express = require('express');
 var router = express.Router();
+// constructor
+function ItemLibrary() {
+  this.items = [];
+  this.id = 0;
+}
+
+// methods
+ItemLibrary.prototype.addItem = function(name) {
+  var newItem = {name: name, id: this.id};
+  this.items.push(newItem);
+  this.id += 1;
+};
+
+// create some instances
+var storage = new ItemLibrary();
+storage.addItem('Noodles');
+storage.addItem('Tomatoes');
+storage.addItem('Peppers');
+
+// route handler
+router.get('/items', function(req, res) {
+  res.json(storage.items);
+});
+
+router.post('/items', function(req, res) {
+  // add code here to create new item instance
+
+});
 
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
